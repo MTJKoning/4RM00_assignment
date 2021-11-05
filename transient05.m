065a9d47698cf69241dcbfb2db9b124a86882d70
M.T.J.Koning <m.t.j.koning@student.tue.nl>
2021-11-05 18:22:15 +0100

    goeie versie dragforce

--- transient05.m
+++ transient05.m
@@ -218,28 +218,28 @@ figure(3)
 surf(X,Y,p);
 
 figure(4)
-plot(Y(2:NPI,6),p(2:NPI,6));
+plot(Y(2:NPI,2),p(2:NPI,2));
 hold on
 plot(Y(2:NPI,NPJ-1),p(2:NPI,NPJ-1));
 hold off
 
 figure(5)
-plot(Y(2:NPI,6),u(2:NPI,6));
+plot(Y(2:NPI,2),u(2:NPI,2));
 hold on
 plot(Y(2:NPI,NPJ-1),u(2:NPI,NPJ-1));
 
 
-p_truck1front=p(15,6);
-p_truck2front=p(ceil(15+NPI_truck + NPI_dis),6);
-p_truck3front=p(ceil(15+2*NPI_truck + 2*NPI_dis),6);
+p_truck1front=p(15,2);
+p_truck2front=p(ceil(15+NPI_truck + NPI_dis),2);
+p_truck3front=p(ceil(15+2*NPI_truck + 2*NPI_dis),2);
 
 p_truck1=p(15,NPJ-1);
 p_truck2=p(ceil(15+NPI_truck + NPI_dis),NPJ-1);
 p_truck3=p(ceil(15+2*NPI_truck + 2*NPI_dis),NPJ-1);
 
-u_truck1front=u(15 -1,6);
-u_truck2front=u(ceil(15+NPI_truck + NPI_dis - 1),6);
-u_truck3front=u(ceil(15+2*NPI_truck + 2*NPI_dis -1),6);
+u_truck1front=u(15 -1,2);
+u_truck2front=u(ceil(15+NPI_truck + NPI_dis - 1),2);
+u_truck3front=u(ceil(15+2*NPI_truck + 2*NPI_dis -1),2);
 
 rho=1.29;
 
@@ -247,9 +247,9 @@ Cp1=(p_truck1front-p_truck1)/(0.5* rho * U_IN);
 Cp2=(p_truck2front-p_truck2)/(0.5* rho * U_IN);
 Cp3=(p_truck3front-p_truck3)/(0.5* rho * U_IN);
 
-Cf1=2*(sqrt((mu(3,3)*dudy(15 -1,6)/rho)/U_IN))^2;
-Cf2=2*(sqrt((mu(3,3)*dudy(ceil(15+NPI_truck + NPI_dis - 1),6))/rho)/U_IN)^2;
-Cf3=2*(sqrt((mu(3,3)*dudy(ceil(15+2*NPI_truck + 2*NPI_dis -1),6))/rho)/U_IN)^2;
+Cf1=2*(sqrt((mu(3,3)*dudy(15 -1,2)/rho)/U_IN))^2;
+Cf2=2*(sqrt((mu(3,3)*dudy(ceil(15+NPI_truck + NPI_dis - 1),2))/rho)/U_IN)^2;
+Cf3=2*(sqrt((mu(3,3)*dudy(ceil(15+2*NPI_truck + 2*NPI_dis -1),2))/rho)/U_IN)^2;
 
 F1=0.5*rho*u_truck1front^2*(Cp1+Cf1);
 F2=0.5*rho*u_truck2front^2*(Cp2+Cf2);
